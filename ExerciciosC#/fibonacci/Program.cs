a836d98060b96ab3f761ab79b474805f34696594
Gustavo <lima.gustavo100@hotmail.com>
2021-05-05 14:28:14 -0300

    Fibonacci

--- ExerciciosC#/fibonacci/Program.cs
+++ ExerciciosC#/fibonacci/Program.cs
@@ -6,7 +6,19 @@ namespace fibonacci
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Fibonacci");
+
+            double nTermo;
+            double termoAnterior = 0;
+            double termoAtual = 1;
+            for (int i = 0; i < 50; i++)
+            {
+                nTermo = termoAnterior + termoAtual;
+                termoAnterior = termoAtual;
+                termoAtual = nTermo;
+
+                Console.WriteLine(termoAnterior);
+            }
         }
     }
 }
