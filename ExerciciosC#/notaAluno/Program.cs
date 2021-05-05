50479462ade0ab48d31b5e24450773b88e2312b7
LimaGustav <lima.gustavo100@homtail.com>
2021-05-05 14:00:24 -0300

    Exercicios senai

---
+++ ExerciciosC#/notaAluno/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace notaAluno
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int nota;
+            do
+            {
+                Console.Write("Digita uma note entre 0 e 10: ");
+                nota = int.Parse(Console.ReadLine());
+                if (nota > 10) {
+                    Console.WriteLine("Nota invalida");
+                }
+            } while (nota > 10);
+        }
+    }
+}
---
+++ ExerciciosC#/validador/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace validador
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   
+            string nome = "";
+            do
+            {
+                Console.Write("Digite um nome: ");
+                nome = Console.ReadLine().Trim();
+
+            } while (nome == "");
+
+            int idade;
+            do
+            {
+                Console.Write("Digite uma idade: ");
+                idade = int.Parse(Console.ReadLine());
+
+            } while (idade > 150 || idade < 0);
+
+            double salario;
+            do
+            {
+                Console.Write("Digite uma salario: R$");
+                salario = Convert.ToInt32(Console.ReadLine());
+
+            } while (salario < 0);
+
+            string estadoCivil = "";
+            do
+            {
+                Console.Write("Digite um estado civil ('s'Solteiro(a)) ('c'Casado(a)) ('v'Viuvo(a)) ('d'Divorciado(a)) ");
+                estadoCivil = Console.ReadLine();
+
+                if (estadoCivil == "s" || estadoCivil == "c" || estadoCivil == "v" || estadoCivil == "d") {
+                    estadoCivil = "s";
+                } else {
+                    estadoCivil = "amendoim";
+                }
+            } while (estadoCivil != "s");
+            Console.Write("Dados validados\nArmazenados com sucesso");
+        }
+    }
+}
